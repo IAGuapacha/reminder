7038d184d396458fff91fdaef5308e5a4e70066b
IAGuapacha <iandresguapacha@gmail.com>
2025-05-24 13:23:02 -0500

    birthday detail screen is added

--- app/src/main/java/com/iaguapacha/reminder/MainActivity.kt
+++ app/src/main/java/com/iaguapacha/reminder/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.iaguapacha.reminder.ui.mainscreen.MainScreen
 import com.iaguapacha.reminder.ui.addbirthday.AddBirthdayScreen
+import com.iaguapacha.reminder.ui.birthdaydetail.BirthdayDetailScreen
 import com.iaguapacha.reminder.ui.theme.ReminderTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,5 +35,12 @@ fun MyApp(){
     NavHost(navController = navController, startDestination = "main") {
         composable("main") { MainScreen(navController) }
         composable("addBirthday") { AddBirthdayScreen(navController) }
+        composable(
+            "birthdayDetail/{contactId}",
+            arguments = listOf(navArgument("contactId") { type = NavType.LongType })
+        ) { backStackEntry ->
+            val contactId = backStackEntry.arguments?.getLong("contactId")
+            BirthdayDetailScreen(navController, contactId ?: 0)
+        }
     }
 }
---
+++ app/src/main/java/com/iaguapacha/reminder/ui/birthdaydetail/BirthdayDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.iaguapacha.reminder.ui.birthdaydetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BirthdayDetailViewModel @Inject constructor(): ViewModel() {
+
+}
